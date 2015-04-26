2521cc6f8d1c4575c4cd1478922a76e3bf7c0d97
Sandeep Ankam <sandeepankam@me.com>
2015-04-26 08:46:08 -0700

    Created a single view controller project.

---
+++ MySpace/MySpace/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySpace
+//
+//  Created by Sandeep Ankam on 4/26/15.
+//  Copyright (c) 2015 SandeepAnkam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MySpace/MySpace/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MySpace
+//
+//  Created by Sandeep Ankam on 4/26/15.
+//  Copyright (c) 2015 SandeepAnkam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
