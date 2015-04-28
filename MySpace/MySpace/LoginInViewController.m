35d288cf8fd875cdda57e3761096cdc8d4c15e65
Sandeep Ankam <sandeepankam@me.com>
2015-04-27 21:04:15 -0700

    Created LoginViewController class and xib file.

---
+++ MySpace/MySpace/LoginInViewController.m
@@ -0,0 +1,45 @@
+//
+//  LoginInViewController.m
+//  MySpace
+//
+//  Created by Sandeep Ankam on 4/27/15.
+//  Copyright (c) 2015 SandeepAnkam. All rights reserved.
+//
+
+#import "LoginInViewController.h"
+
+@interface LoginInViewController ()
+
+@end
+
+@implementation LoginInViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)signInButtonPressed:(id)sender {
+}
+
+- (IBAction)signUpButtonPressed:(id)sender {
+}
+
+- (IBAction)forgotPasswordButtonPressed:(id)sender {
+}
+@end
