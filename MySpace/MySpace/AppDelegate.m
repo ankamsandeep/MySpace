8699366e99093e8c00d9e2c3caac1d4c8aa34f5e
Kapil Rathan <KapilRathan@Kapils-MacBook-Pro.local>
2015-05-01 23:46:11 -0400

    Added Registration, Notifications, Signout, home screens

--- MySpace/MySpace/AppDelegate.m
+++ MySpace/MySpace/AppDelegate.m
@@ -15,8 +15,16 @@
 @implementation AppDelegate
 
 
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+        self.viewController = [[ViewController alloc] initWithNibName:@"LoginScreen" bundle:nil];
+    [self.window setRootViewController:self.viewController];
+    
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
