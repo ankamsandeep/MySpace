731dd946a10a4a91637f5794f765cb10a26b0fda
Sandeep Ankam <sandeepankam@me.com>
2015-05-02 09:04:43 -0700

    Added Frameworks

---
+++ MySpace/MySpace/RegistrationViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegistrationViewController.h
+//  MySpace
+//
+//  Created by Sandeep Ankam on 5/2/15.
+//  Copyright (c) 2015 SandeepAnkam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegistrationViewController : UIViewController
+
+@end
