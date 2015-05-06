d09cfd58c289b403a76ec940416e81c74df7c48b
NickeyDev <reddy.nishanth05@gmail.com>
2015-05-05 19:11:03 -0700

    created navigation and tabbar controllers

---
+++ MySpace/MySpace/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  MySpace
+//
+//  Created by nishanth reddy kuninti on 5/5/15.
+//  Copyright (c) 2015 SandeepAnkam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ MySpace/MySpace/SignoutViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignoutViewController.h
+//  MySpace
+//
+//  Created by nishanth reddy kuninti on 5/5/15.
+//  Copyright (c) 2015 SandeepAnkam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignoutViewController : UIViewController
+
+@end
