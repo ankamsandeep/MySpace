5ee98e50f0510b9f06ba4908cdf5ad879fbfc5f4
Kapil Rathan <KapilRathan@Kapils-MacBook-Pro.local>
2015-05-12 01:59:38 -0400

    completed code with error in Settings page

---
+++ MySpace/MySpace/MySpaceAudioBook.m
@@ -0,0 +1,13 @@
+//
+//  MySpaceAudioBook.m
+//  MySpace
+//
+//  Created by Kapil Rathan on 5/12/15.
+//  Copyright (c) 2015 SandeepAnkam. All rights reserved.
+//
+
+#import "MySpaceAudioBook.h"
+
+@implementation MySpaceAudioBook
+
+@end
---
+++ MySpace/MySpace/MySpaceDataRequestor.h
@@ -0,0 +1,13 @@
+//
+//  MySpaceDataRequestor.h
+//  MySpace
+//
+//  Created by Kapil Rathan on 5/12/15.
+//  Copyright (c) 2015 SandeepAnkam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MySpaceDataRequestor : NSObject
+
+@end
